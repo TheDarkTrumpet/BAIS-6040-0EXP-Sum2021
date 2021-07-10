cdaa48b85b0d7d908a7bafba7d14e6fdeac906ea
David Thole <david@thedarktrumpet.com>
2021-07-10 15:29:48 -0500

    Finishing the sql script, reinitializing the database

--- RealWorld/AdvancedFlask/create_tables.sql
+++ RealWorld/AdvancedFlask/create_tables.sql
@@ -5,4 +5,32 @@ create table People
     lname varchar(255),
     address varchar(500),
     phone varchar(12)
-)
+);
+
+insert into People(fname, lname, address, phone) values (
+'First Name A',
+'Last Name A',
+'1122 Address A',
+'121-121-1234'
+);
+
+insert into People(fname, lname, address, phone) values (
+'First Name B',
+'Last Name B',
+'1122 Address B',
+'121-121-1234'
+);
+
+insert into People(fname, lname, address, phone) values (
+'First Name C',
+'Last Name C',
+'1122 Address C',
+'121-121-1234'
+);
+
+insert into People(fname, lname, address, phone) values (
+'First Name D',
+'Last Name D',
+'1122 Address D',
+'121-121-1234'
+);
